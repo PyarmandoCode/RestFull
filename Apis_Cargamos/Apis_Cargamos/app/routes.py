8cef63cbc951dc7e829234a5084c6db79038fae0
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-14 14:51:40 -0500

    detail

--- Apis_Cargamos/Apis_Cargamos/app/routes.py
+++ Apis_Cargamos/Apis_Cargamos/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template,make_response,jsonify
 from app import app,db
 from app.models import Grupos
-from app.serializer import grupos_schema
+from app.serializer import grupos_schema,grupo_schema
 from flask import request
 
 @app.route('/')
@@ -29,6 +29,25 @@ def listar_grupos():
         'data':result
     }
     return make_response(jsonify(data))
+
+@app.route("/detalle_grupo/<int:id>",methods=["GET"])
+def detalle_grupo(id):
+    #todo para seleccionar solo un objeto por id
+    grupo=Grupos.query.get(id)
+    if grupo:
+        result=grupo_schema.dump(grupo)        
+        data= {
+        'message':'1',
+        'status':200,
+        'data':result
+            }
+    else:
+        data = {
+            'message':'0',
+            'status':200,
+        }
+    return make_response(jsonify(data))
+    
 
 
 
