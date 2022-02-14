a4149e351fdadad8b1b6d83e986ef55ebb457d94
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-14 15:19:33 -0500

    add

--- Apis_Cargamos/Apis_Cargamos/app/models.py
+++ Apis_Cargamos/Apis_Cargamos/app/models.py
@@ -4,3 +4,9 @@ class Grupos(db.Model):
     codgrupo=db.Column(db.Integer,primary_key=True,autoincrement=True)
     nombre_grupo=db.Column(db.String(50))
     estado=db.Column(db.Integer)
+    
+    #todo crear constructor cuando se desea pasar valores
+    #todo al servicio
+    def __init__(self,nombre_grupo,estado):
+        self.nombre_grupo=nombre_grupo
+        self.estado=estado 
--- Apis_Cargamos/Apis_Cargamos/app/routes.py
+++ Apis_Cargamos/Apis_Cargamos/app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template,make_response,jsonify
+
+from flask import render_template,make_response,jsonify,request
 from app import app,db
 from app.models import Grupos
 from app.serializer import grupos_schema,grupo_schema
-from flask import request
+
 
 @app.route('/')
 def index():
@@ -35,6 +36,8 @@ def detalle_grupo(id):
     #todo para seleccionar solo un objeto por id
     grupo=Grupos.query.get(id)
     if grupo:
+        #todo dump permite serializar los objetos en PYTHON
+        #todo como por ejemplo convertir un STR en json
         result=grupo_schema.dump(grupo)        
         data= {
         'message':'1',
@@ -47,7 +50,29 @@ def detalle_grupo(id):
             'status':200,
         }
     return make_response(jsonify(data))
+
+@app.route("/add_grupo",methods=["POST"])
+def add_grupo():
+    #todo request tomar los parametros qe se insertarn en
+    #todo la tablas
+    nombre_grupo=request.json['nombre_grupo']
+    estado=request.json['estado']
     
+    new_grupo = Grupos(nombre_grupo,estado)
+    #todo se guarda el objeto en la session del usuario
+    db.session.add(new_grupo)
+    #todo se guarda fisicamente en la tabla
+    db.session.commit()
+    #todo esto permitira que el front vea cual es el registro
+    #todo que se inserto
+    result=grupo_schema.dump(new_grupo)
+    
+    data = {
+        'message':'Se Agrego con exito',
+        'status':200,
+        'data':result
+    }
+    return make_response(jsonify(data))
 
 
 
