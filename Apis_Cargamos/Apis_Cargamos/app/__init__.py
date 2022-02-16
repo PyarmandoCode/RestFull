7e16cf7d48dbac422409c34dcbff943cf72b6de3
armando.eu.ruiz@gmail.com <armando.eu.ruiz@gmail.com>
2022-02-16 12:45:44 -0500

    make it better

--- Apis_Cargamos/Apis_Cargamos/app/__init__.py
+++ Apis_Cargamos/Apis_Cargamos/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 app.config.from_object(Config)
 db=SQLAlchemy(app)
 
